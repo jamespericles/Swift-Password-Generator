b71c5ae484fa13a764da2fd075e1c62b48702822
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-05 18:50:58 -0400

    initial commit

---
+++ Password Generator/Password Generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Password Generator
+//
+//  Created by James Pericles II on 7/5/20.
+//  Copyright © 2020 James Pericles II. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
