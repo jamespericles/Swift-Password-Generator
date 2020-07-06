a1fa178296ccf0a5b05a10261d8b3805ab067405
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-06 01:42:19 -0400

    slider now displays live

--- Password Generator/Password Generator/ViewController.swift
+++ Password Generator/Password Generator/ViewController.swift
@@ -2,23 +2,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+//    Variables containing the different possible characters
     let lowercase = "abcdefghijklmnopqrstuvwxyz",
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers = "0123456789",
     specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-="
     
     
+    
+//    Pull the switches from the storyboard
     @IBOutlet weak var lowercaseSwitch: UISwitch!
     @IBOutlet weak var uppercaseSwitch: UISwitch!
     @IBOutlet weak var numberSwitch: UISwitch!
     @IBOutlet weak var specialSwitch: UISwitch!
+//    All elements related to the slider and displaying its length
+    @IBOutlet weak var sliderLength: UISlider!
+    @IBOutlet weak var sliderLengthDisplay: UILabel!
+    
+    @IBOutlet weak var passwordDisplay: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
     
-    
+//     Generate function
     @IBAction func generateButtonPressed(_ sender: UIButton) {
         
         var passwordCharSet = ""
@@ -41,7 +50,12 @@ class ViewController: UIViewController {
         
        print(passwordCharSet)
     }
-    
+//    Update the length of the slider live
+    @IBAction func updateSliderLength(_ sender: UISlider) {
+          
+        sliderLengthDisplay.text = "\(sender.value)"
+          
+      }
     
     
 }
