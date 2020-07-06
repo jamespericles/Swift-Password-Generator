57e73e06a92b6700a2b9019bf6b193c87f3a65d6
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-06 00:33:06 -0400

    complete rehaul of project, made a lot of beginner mistakes that have since been fixed

--- Password Generator/Password Generator/ViewController.swift
+++ Password Generator/Password Generator/ViewController.swift
@@ -1,65 +1,27 @@
-//
-//  ViewController.swift
-//  Password Generator
-//
-//  Created by James Pericles II on 7/5/20.
-//  Copyright © 2020 James Pericles II. All rights reserved.
-//
-
 import UIKit
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-        
-        
-        
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-        
-        
-        
-    }
-}
 
 class ViewController: UIViewController {
-
+    
+    
+   @IBOutlet weak var lowercaseSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func lowercaseCharacterSwitch(_ sender: UISwitch) {
+    
+    @IBAction func generateButtonPressed(_ sender: UIButton) {
+        lowercaseSwitch.isOn
+      
+        print("button pressed")
+        
     }
     
     
-    @IBAction func uppercaseCharacterSwitch(_ sender: UISwitch) {
-    }
     
-    
-    @IBAction func numberSwitch(_ sender: UISwitch) {
-    }
-
-    
-    @IBAction func specialCharacterSwitch(_ sender: UISwitch) {
-    }
-    
-    
-    @IBAction func passwordLengthSlider(_ sender: UISlider) {
-    }
-    
-    
-    @IBAction func generateButton(_ sender: UIButton) {
-    }
-    
-    
-    @IBAction func copyButton(_ sender: UIButton) {
-    }
 }
+
 
